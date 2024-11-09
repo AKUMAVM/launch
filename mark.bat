74944b172f6ccc52aa21c79df1bfcbda547c3294
AKUMAVM <admin@akumavm.com>
2024-11-09 12:08:46 +0700

    Update mark.bat

--- mark.bat
+++ mark.bat
@@ -3,7 +3,6 @@ mode con cp select=437 >nul
 setlocal EnableDelayedExpansion
 
 set confhome=https://raw.githubusercontent.com/AKUMAVM/launch/main
-#set confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/bin456789/reinstall/main
 rem set confhome_cn=https://jihulab.com/bin456789/reinstall/-/raw/main
 
 rem 65001 代码页会乱码
