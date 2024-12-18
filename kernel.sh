21a4c9c7b27295ec36c488588d8639fb5cf63d54
AKUMAVM <admin@akumavm.com>
2024-12-18 12:25:49 +0700

    Create kernel.sh

---
+++ kernel.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+if [[ $EUID -ne 0 ]]; then
+    clear
+    echo "Error: This script must be run as root!" 1>&2
+    exit 1
+fi
+
+if [ -f "/usr/bin/yum" ] && [ -d "/etc/yum.repos.d" ]; then
+    yum update -y
+    yum install -y wget curl
+    
+elif [ -f "/usr/bin/apt-get" ] && [ -f "/usr/bin/dpkg" ]; then
+    apt-get update --fix-missing
+    apt install -y curl wget
+    
+fi
+
+function CopyRight() {
+  clear
+  echo "########################################################"
+  echo "#                                                      #"
+  echo "#               Auto Reinstall Script                  #"
+  echo "#                                                      #"
+  echo "#               Author: AkumaVM                        #"
+  echo "#               Last Modified: 18-12-2024              #"
+  echo "#                                                      #"
+  echo "#               Supported by bin456789                 #"
+  echo "#                                                      #"
+  echo "########################################################"
+  echo -e "\n"
+}
+
+curl -O https://raw.githubusercontent.com/AKUMAVM/launch/main/mark.sh || wget -O mark.sh $_
+
+
+  echo -e "\nPlease select an OS:"
+  echo "  1) Latest Debian"
+  echo "  2) Latest Ubuntu"
+  echo "  3) Latest Almalinux"
+  echo "  4) Latest Alpine Linux"
+  echo "  5) Latest CentOS"
+  echo "  6) Kali Linux"
+  echo "  7) Windows 10 Pro"
+  echo "  8) Windows 11 Pro"
+  echo "  9) Windows Server 2012 R2 DC"
+  echo "  10) Windows Server 2016 DC"
+  echo "  11) Windows Server 2019 DC"
+  echo "  12) Windows Server 2022 DC"
+  echo "  13) Windows Server 2025 DC"
+  echo "  14) Netboot"
+  echo "  99) Custom DD image"
+  echo "  0)Exit"
+  echo -ne "\nYour option: "
+  read N
+  case $N in
+    1) bash mark.sh debian --password 123@@@ ;;
+    2) bash mark.sh ubuntu --password 123@@@ ;;
+    3) bash mark.sh alma --password 123@@@ ;;
+    4) bash mark.sh alpine --password 123@@@ ;;
+    5) bash mark.sh centos --password 123@@@ ;;
+    6) bash mark.sh kali --password 123@@@ ;;
+    7) bash mark.sh --image-name-'Windows 10 Pro' --lang=en-us --password 123@@@ ;;
+    8) bash mark.sh --image-name-'Windows 11 Pro' --lang=en-us --password 123@@@ ;;
+    9) bash mark.sh --image-name-'Windows Server 2012 R2 SERVERDATACENTER' --lang=en-us --password 123@@@ ;;
+    10) bash mark.sh --image-name-'Windows Server 2016 SERVERDATACENTER' --lang=en-us --password 123@@@ ;;
+    11) bash mark.sh --image-name-'Windows Server 2019 SERVERDATACENTER' --lang=en-us --password 123@@@ ;;
+    12) bash mark.sh --image-name-'Windows Server 2022 SERVERDATACENTER' --lang=en-us --password 123@@@ ;;
+    13) bash mark.sh --image-name-'Windows Server 2025 SERVERDATACENTER' --lang=en-us --password 123@@@ ;;
+    14) bash mark.sh netboot.xyz ;;
+    99)
+      echo -e "\n"
+      read -r -p "Custom DD image URL: " imgURL
+      echo -e "\n"
+      read -r -p "Are you sure start reinstall? [y/N]: " input
+      case $input in
+        [yY][eE][sS]|[yY]) bash mark.sh dd --img $imgURL ;;
+        *) clear; echo "Canceled by user!"; exit 1;;
+      esac
+      ;;
+    0) exit 0;;
+    *) echo "Wrong input!"; exit 1;;
+esac
+}
