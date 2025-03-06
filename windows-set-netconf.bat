12ccf6a2f42b8e41bb2800272d9ed903428b8265
AKUMAVM <admin@akumavm.com>
2025-03-07 00:00:08 +0700

    Updates core: windows-set-netconf.bat

--- windows-set-netconf.bat
+++ windows-set-netconf.bat
@@ -1,88 +1,62 @@
-rem set mac_addr=11:22:33:aa:bb:cc
-
-rem set ipv4_addr=192.168.1.2/24
-rem set ipv4_gateway=192.168.1.1
-rem set ipv4_dns1=192.168.1.1
-rem set ipv4_dns2=192.168.1.2
-
-rem set ipv6_addr=2222::2/64
-rem set ipv6_gateway=2222::1
-rem set ipv6_dns1=::1
-rem set ipv6_dns2=::2
-
 @echo off
 mode con cp select=437 >nul
 setlocal EnableDelayedExpansion
 
-rem 禁用 IPv6 地址标识符的随机化，防止 IPv6 和后台面板不一致
+rem Define MAC address
+set mac_addr=11:22:33:aa:bb:cc
+
+rem Define IPv4 settings
+set ipv4_addr=192.168.1.2/24
+set ipv4_gateway=192.168.1.1
+
+rem Define IPv6 settings
+set ipv6_addr=2222::2/64
+set ipv6_gateway=2222::1
+
+rem Disable IPv6 address randomization
 netsh interface ipv6 set global randomizeidentifiers=disabled
 
-rem 检查是否定义了 MAC 地址
+rem Find network interface ID based on MAC address
 if defined mac_addr (
     for /f %%a in ('wmic nic where "MACAddress='%mac_addr%'" get InterfaceIndex ^| findstr [0-9]') do set id=%%a
     if defined id (
-        rem 配置静态 IPv4 地址和网关
+        rem Configure static IPv4 address and gateway
         if defined ipv4_addr if defined ipv4_gateway (
-        rem gwmetric 默认值为 1，自动跃点需设为 0
             netsh interface ipv4 set address !id! static !ipv4_addr! gateway=!ipv4_gateway! gwmetric=0
         )
 
-        rem 配置静态 IPv4 DNS 服务器
-        for %%i in (1, 2) do (
-            if defined ipv4_dns%%i (
-                netsh interface ipv4 add | findstr "dnsservers"
-                if ErrorLevel 1 (
-                    rem vista
-                    netsh interface ipv4 add dnsserver !id! !ipv4_dns%%i! %%i
-                ) else (
-                    rem win7
-                    netsh interface ipv4 add dnsservers !id! !ipv4_dns%%i! %%i no
-                )
-            )
-        )
+        rem Set static IPv4 DNS to Cloudflare and Google
+        netsh interface ipv4 set dnsserver !id! static 1.1.1.1 primary
+        netsh interface ipv4 add dnsserver !id! 8.8.8.8 index=2
 
-        rem 配置 IPv6 地址和网关
+        rem Configure IPv6 address and gateway
         if defined ipv6_addr if defined ipv6_gateway (
             netsh interface ipv6 set address !id! !ipv6_addr!
-            netsh interface ipv6 add route prefix=::/0 !id! !ipv6_gateway!
+            netsh interface ipv6 add route ::/0 !id! !ipv6_gateway!
         )
 
-        rem 配置 IPv6 DNS 服务器
-        for %%i in (1, 2) do (
-            if defined ipv6_dns%%i (
-                netsh interface ipv6 add | findstr "dnsservers"
-                if ErrorLevel 1 (
-                    rem vista
-                    netsh interface ipv6 add dnsserver !id! !ipv6_dns%%i! %%i
-                ) else (
-                    rem win7
-                    netsh interface ipv6 add dnsservers !id! !ipv6_dns%%i! %%i no
-                )
-            )
-        )
+        rem Set static IPv6 DNS to Cloudflare and Google
+        netsh interface ipv6 set dnsserver !id! static 2606:4700:4700::1111 primary
+        netsh interface ipv6 add dnsserver !id! 2001:4860:4860::8888 index=2
     )
 )
 
-REM Downloading Qemu Agent
+REM Download and install Qemu Agent
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.262-2/virtio-win-guest-tools.exe', 'C:\Windows\Temp\virtio-win-guest-tools.exe')" <NUL
 cmd /c C:\Windows\Temp\virtio-win-guest-tools.exe /quiet /norestart
 del C:\Windows\Temp\virtio-win-guest-tools.exe
 
-REM Check for OneDrive setup executable path
+REM Remove OneDrive
 set onedrive=%SystemRoot%\SysWOW64\OneDriveSetup.exe
 if not exist "%onedrive%" (
     set onedrive=%SystemRoot%\System32\OneDriveSetup.exe
 )
 
-REM Stop OneDrive processes
 taskkill /F /IM OneDrive.exe /T
 timeout /t 2 > nul
-
-REM Uninstall OneDrive
 "%onedrive%" /uninstall
 timeout /t 2 > nul
 
-REM Remove leftover OneDrive files
 rmdir "%USERPROFILE%\OneDrive" /S /Q
 rmdir "%LOCALAPPDATA%\Microsoft\OneDrive" /S /Q
 rmdir "%PROGRAMDATA%\Microsoft OneDrive" /S /Q
@@ -90,44 +64,22 @@ if exist "%SYSTEMDRIVE%\OneDriveTemp" (
     rmdir "%SYSTEMDRIVE%\OneDriveTemp" /S /Q
 )
 
-#REM Cleanup Profile Usage Information
-#for /d %%D in ("C:\Users\Administrator\AppData\Local\Temp\*") do rd /s /q "%%D"
-#del /q /f "C:\Users\Administrator\AppData\Local\Temp\*"
-#for /d %%D in ("C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Recent\*") do rd /s /q "%%D"
-#del /q /f "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Recent\*"
-#for /d %%D in ("C:\Users\Administrator\Downloads\*") do rd /s /q "%%D"
-#del /q /f "C:\Users\Administrator\Downloads\*"
-#del /q /f "C:\Windows\System32\sysprep\Panther\setupact.log"
-#del /q /f "C:\Windows\System32\sysprep\Panther\setuperr.log"
-#del /q /f "C:\Windows\System32\sysprep\Panther\IE\setupact.log"
-
-#REM Removes Temporary Files
-#for /d %%D in ("C:\Windows\Temp\*") do rd /s /q "%%D"
-#del /q /f "C:\Windows\Temp\*"
-
-#REM Clears Explorer Run History
-#reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" /f >nul 2>&1
-
-REM Removes any previous Memory Dump files
+REM Remove memory dump files
 del /q /f "C:\Windows\*.DMP"
 for /d %%D in ("C:\Windows\Minidump") do rd /s /q "%%D"
 
-#REM Clearing Event Logs
-#wevtutil cl Application
-#wevtutil cl System
-#wevtutil cl Security
-
-REM Downloading Optimize Apps
+REM Download and run system optimizer
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://install.virtfusion.net/optimize.exe', 'C:\Windows\Temp\optimize.exe')" <NUL
 cmd /c C:\Windows\Temp\optimize.exe -v -o -g -windowsupdate disable -storeapp remove-all -antivirus disable
 cmd /c C:\Windows\Temp\optimize.exe -f 3 4 5 6 9
 del C:\Windows\Temp\optimize.exe
 
-REM Set the account lockout threshold to 0 (disabled)
+REM Set account lockout threshold to 0 (disable)
 net accounts /lockoutthreshold:0
-
-REM Confirm the setting
 net accounts | find /i "Lockout threshold"
 
-rem Delete script file
+REM Restart the server after execution
+shutdown /r /t 10 /f
+
+rem Delete script file after execution
 del "%~f0"
